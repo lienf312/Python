ebb3e9f75b32f38a2f0425cbcb05f48f6b2f2424
Jelena Kovaljova tthk <jel.kovaljova@gmail.com>
2025-03-20 15:07:46 +0200

    Update Mäng "Kivi-paber-käärid".py

--- Mäng "Kivi-paber-käärid".py
+++ Mäng "Kivi-paber-käärid".py
@@ -1,9 +1,9 @@
 import random
 
-player1 = "Человек"
-player2 = "Робот"
+player1 = "inimene"
+player2 = "Robot"
 
-choices = ["Камень", "Ножницы", "Бумага"]
+choices = ["kivi", "Käärid", "Paber"]
 
 player1_score = 0
 player2_score = 0
@@ -11,38 +11,38 @@ player2_score = 0
 rounds = 5
 
 for round_num in range(1, rounds + 1):
-    print(f"Раунд {round_num}")
+    print(f"rounds {round_num}")
     
     # человек
-    player1_choice = input("Ваш выбор (Камень, Ножницы, Бумага): ").capitalize()
+    player1_choice = input("Teie valik (Кivi, Käärid, Paber): ").capitalize()
     while player1_choice not in choices:
-        print("Неверный ввод! Попробуйте снова.")
-        player1_choice = input("Ваш выбор (Камень, Ножницы, Бумага): ").capitalize()
+        print("Viga! Proovi uuesti.")
+        player1_choice = input("Teie valik (Кivi, Käärid, Paber): ").capitalize()
     
     # робот 
     player2_choice = random.choice(choices)
-    print(f"Робот выбрал: {player2_choice}")
+    print(f"Robot võtis: {player2_choice}")
     
     # победитель
     if player1_choice == player2_choice:
-        result = "Ничья"
-    elif (player1_choice == "Камень" and player2_choice == "Ножницы") or \
-         (player1_choice == "Ножницы" and player2_choice == "Бумага") or \
-         (player1_choice == "Бумага" and player2_choice == "Камень"):
-        result = "Победа игрока 1"
+        result = "viik"
+    elif (player1_choice == "Kivi" and player2_choice == "Käärid") or \
+         (player1_choice == "Käärid" and player2_choice == "Paber") or \
+         (player1_choice == "Paber" and player2_choice == "Kivi"):
+        result = "Mängija 1 võit"
         player1_score += 1
     else:
-        result = "Победа игрока 2"
+        result = "Mängija 2 võit"
         player2_score += 1
 
-    print(f"Результат: {result}")
+    print(f"Tulemus: {result}")
 
 # результаты
-print(f"\nИтоговый счёт: {player1} - {player1_score} очков, {player2} - {player2_score} очков")
+print(f"\nLõpptulemus: {player1} - {player1_score} очков, {player2} - {player2_score} punktid")
 
 if player1_score > player2_score:
-    print(f"{player1} победил!")
+    print(f"{player1} võitis!")
 elif player2_score > player1_score:
-    print(f"{player2} победил!")
+    print(f"{player2} võitis!")
 else:
-    print("Ничья!")
+    print("Viik!")
